562045f14f141c66f422e364c23d2008f293d0ab
Jules Merigot <77292264+jmerigot@users.noreply.github.com>
2023-11-11 18:21:10 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -22,6 +22,7 @@ def D_train(x, L_G, G, D, D_optimizer, criterion):
 
     # train discriminator on fake
     # z = torch.randn(x.shape[0], 100).cuda()
+    print(f"Shape of x: {x.shape[0]}")
     z = L_G(batch_size=x.shape[0]).cuda()
     print(f"Shape of z before G: {z.shape}")  # New print statement
     x_fake, y_fake = G(z).cuda(), torch.zeros(x.shape[0], 1).cuda()
