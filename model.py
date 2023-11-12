05d726ad1eef55809b7367b82b7c7c52b7ff578a
alexandrengau <145470229+alexandrengau@users.noreply.github.com>
2023-11-12 19:36:10 +0100

    Update model.py

--- model.py
+++ model.py
@@ -83,6 +83,23 @@ class Discriminator(nn.Module):
     def __init__(self, d_input_dim):
         super(Discriminator, self).__init__()
         self.fc1 = nn.Linear(d_input_dim, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 1)
+
+    def forward(self, x):
+        x = x.view(-1, self.fc1.in_features)  # Flatten the input if needed
+        x = F.leaky_relu(self.fc1(x), 0.2)
+        x = F.leaky_relu(self.fc2(x), 0.2)
+        x = F.leaky_relu(self.fc3(x), 0.2)
+        x = self.fc4(x)
+        return torch.sigmoid(x)
+
+"""
+class Discriminator(nn.Module):
+    def __init__(self, d_input_dim):
+        super(Discriminator, self).__init__()
+        self.fc1 = nn.Linear(d_input_dim, 1024)
         self.fc2 = nn.Linear(self.fc1.out_features, self.fc1.out_features//2)
         self.fc3 = nn.Linear(self.fc2.out_features, self.fc2.out_features//2)
         self.fc4 = nn.Linear(self.fc3.out_features, 1)
@@ -93,3 +110,4 @@ class Discriminator(nn.Module):
         x = F.leaky_relu(self.fc2(x), 0.2)
         x = F.leaky_relu(self.fc3(x), 0.2)
         return torch.sigmoid(self.fc4(x))
+"""
