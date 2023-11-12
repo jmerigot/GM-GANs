1367695f2d0e92df00b67ab7909b8c6bd083f0ec
alexandrengau <145470229+alexandrengau@users.noreply.github.com>
2023-11-12 21:10:36 +0100

    Update model.py

--- model.py
+++ model.py
@@ -80,6 +80,21 @@ class Generator(nn.Module):
         return torch.tanh(self.fc4(x))
 
 """
+def __init__(self, d_input_dim):
+        super(Discriminator, self).__init__()
+        self.fc1 = nn.Linear(d_input_dim, 1024)
+        self.fc2 = nn.Linear(self.fc1.out_features, self.fc1.out_features//2)
+        self.fc3 = nn.Linear(self.fc2.out_features, self.fc2.out_features//2)
+        self.fc4 = nn.Linear(self.fc3.out_features, 1)
+
+    # forward method
+    def forward(self, x):
+        x = F.leaky_relu(self.fc1(x), 0.2)
+        x = F.leaky_relu(self.fc2(x), 0.2)
+        x = F.leaky_relu(self.fc3(x), 0.2)
+        return torch.sigmoid(self.fc4(x))
+"""
+
 class Discriminator(nn.Module):
     def __init__(self, d_input_dim, dropout_prob=0.3):
         super(Discriminator, self).__init__()
@@ -101,28 +116,4 @@ class Discriminator(nn.Module):
         x = self.dropout3(x)
         x = self.fc4(x)
         return torch.sigmoid(x)
-"""
-
-class Discriminator(nn.Module):
-    def __init__(self, d_input_dim, dropout_prob=0.3):
-        super(Discriminator, self).__init__()
-        self.fc1 = nn.Linear(d_input_dim, 1024)
-        self.dropout1 = nn.Dropout(dropout_prob)
-        self.fc2 = nn.Linear(self.fc1.out_features, self.fc1.out_features//2)
-        self.dropout2 = nn.Dropout(dropout_prob)
-        self.fc3 = nn.Linear(self.fc2.out_features, self.fc2.out_features//2)
-        self.dropout3 = nn.Dropout(dropout_prob)
-        self.fc4 = nn.Linear(self.fc3.out_features, 1)
-
-    # forward method
-    def forward(self, x):
-        x = x.view(-1, self.fc1.in_features)
-        x = F.leaky_relu(self.fc1(x), 0.2)
-        x = self.dropout1(x)
-        x = F.leaky_relu(self.fc2(x), 0.2)
-        x = self.dropout2(x)
-        x = F.leaky_relu(self.fc3(x), 0.2)
-        x = self.dropout3(x)
-        x = self.fc4(x)
-        return torch.sigmoid(self.fc4(x))
 
