615e28ca10d818718b2897b4d73418b88113cf6d
alexandrengau <145470229+alexandrengau@users.noreply.github.com>
2023-11-12 18:01:14 +0100

    Update model.py

--- model.py
+++ model.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.categorical import Categorical
 
-torch.manual_seed(24)
-torch.cuda.manual_seed(24)
+torch.manual_seed(44)
+torch.cuda.manual_seed(44)
 
 
 class Latent_Generator(torch.nn.Module):
