e5cfa8ddb70777036c878809131a9df150a871f6
alexandrengau <145470229+alexandrengau@users.noreply.github.com>
2023-11-12 19:43:54 +0100

    Update model.py

--- model.py
+++ model.py
@@ -80,18 +80,24 @@ class Generator(nn.Module):
         return torch.tanh(self.fc4(x))
 
 class Discriminator(nn.Module):
-    def __init__(self, d_input_dim):
+    def __init__(self, d_input_dim, dropout_prob=0.5):
         super(Discriminator, self).__init__()
         self.fc1 = nn.Linear(d_input_dim, 1024)
+        self.dropout1 = nn.Dropout(dropout_prob)
         self.fc2 = nn.Linear(1024, 512)
+        self.dropout2 = nn.Dropout(dropout_prob)
         self.fc3 = nn.Linear(512, 256)
+        self.dropout3 = nn.Dropout(dropout_prob)
         self.fc4 = nn.Linear(256, 1)
 
     def forward(self, x):
-        x = x.view(-1, self.fc1.in_features)  # Flatten the input if needed
+        x = x.view(-1, self.fc1.in_features)
         x = F.leaky_relu(self.fc1(x), 0.2)
+        x = self.dropout1(x)
         x = F.leaky_relu(self.fc2(x), 0.2)
+        x = self.dropout2(x)
         x = F.leaky_relu(self.fc3(x), 0.2)
+        x = self.dropout3(x)
         x = self.fc4(x)
         return torch.sigmoid(x)
 
